ac80b46db070177d559ed4e343486d45ca39bbe6
Md Nur Islam <nurislamimran23@gmail.com>
2024-12-20 03:22:38 +0600

    problem solving--class-22__JS-04

---
+++ number.js
@@ -0,0 +1,104 @@
+// Question 1 solve start
+
+const min_number = Math.min(10, 20, 30, 40, 50, 60, 700);
+console.log("min_number=", min_number);
+
+const max_number = Math.max(10, 20, 30, 40, 50, 60, 700);
+console.log("max_number=", max_number);
+// Question 1 solve end
+
+// Question 2 solve start
+
+var Number = 50;
+
+if (Number > 0) {
+  console.log("number is positive");
+} else if (Number < 0) {
+  console.log("number is negetive");
+} else {
+  console.log("number is zero");
+}
+// Question 2 solve end
+
+// Question 3 solve start
+
+let number = 100;
+
+if (number % 10 == 0) {
+  console.log("number is divisible");
+} else {
+  console.log("number is no divisible");
+}
+// Question 3 solve end
+
+// Question 4 solve start
+
+let check_value = 50;
+
+if (check_value % 2 == 0) {
+  console.log("its even");
+} else {
+  console.log("itd odd");
+}
+// Question 4 solve end
+
+// Question 5 solve start
+
+var character = "10";
+if (character >= "a" == character <= "z") {
+  console.log("its alphabet");
+} else {
+  console.log("its not alphabet");
+}
+// Question 5 solve end
+
+// Question 6 solve start
+
+var weather = 10;
+
+if (weather >= 30) {
+  console.log("hot");
+} else if (weather >= 11) {
+  console.log("normal");
+} else if (weather <= 10) {
+  console.log("cold");
+}
+// Question 6 solve end
+
+// Question 7 solve start
+
+let num_1 = 500;
+let num_2 = 200;
+let num_3 = 300;
+
+let maximum_number = Math.max(num_1, num_2, num_3);
+console.log("maximum number=:", maximum_number);
+// Question 7 solve end
+
+// Question 8 solve start
+let X = 105;
+let results = X % 2 == 0 ? "even number" : "odd number";
+console.log(results);
+// Question 8 solve end
+
+// Question 9 solve start
+
+let NumOne = 100;
+let NumTwo = 200;
+
+if (NumOne > 30 && NumTwo > 30) {
+  console.log("all is gather from 30");
+} else {
+  console.log("number is not gather");
+}
+// Question 9 solve end
+
+// Question 10 solve start
+
+let age = 13;
+
+if (age >= 19 == age >= 13) {
+  console.log("teenager");
+} else {
+  console.log("no teenager");
+}
